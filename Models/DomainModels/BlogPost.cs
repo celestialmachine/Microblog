a600bbfada903306af6e2444bc2aa480cb57788e
Victor Diaz <master_v12@hotmail.com>
2023-05-17 21:13:20 -0700

    Added initial folder structure and starter mvc files

---
+++ Models/DomainModels/BlogPost.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Microblog.Models
+{
+    public class BlogPost
+    {
+        //TODO work on data validation ruels
+        public int BlogPostId { get; set; }
+        [Required(ErrorMessage = "Please enter a title for the blog post.")]
+        [Range(0, 64)]
+        public string? Title { get; set; }
+        [Required]
+        public string? Content { get; set; }
+        public DateTime CreatedDate { get; set;}
+        public DateTime UpdatedDate { get; set;}
+        public string? Category { get; set; }
+        public int WordCount(string post)
+        {
+            //TODO this does not account for double spaces and maybe
+            //not markdown either, find better solution
+            return post.Split(" ").Length;
+        }
+    }
+}
--- Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace Microblog.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
