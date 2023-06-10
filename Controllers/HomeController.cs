a8f8d0e5aaba113c962ccb5ebec2579e9cf3bddf
Victor Diaz <master_v12@hotmail.com>
2023-06-10 14:34:25 -0700

    Added jquery ajax function, added LoadMore action to home controller

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -9,6 +9,7 @@ namespace Microblog.Controllers
     {
         private MicroblogContext _context { get; set; }
         public HomeController(MicroblogContext context) => _context = context;
+        private int _pageSize = 4;
 
         [HttpGet]
         [Route("/{FilterCategory?}")]
@@ -38,6 +39,23 @@ namespace Microblog.Controllers
             //query = query.Skip((vm.CurrentRoute.PageNumber - 1) * vm.CurrentRoute.PageSize).Take(vm.CurrentRoute.PageSize);
             vm.Posts = query.ToList();
             return View(vm);
+        }
+
+        [HttpGet]
+        public JsonResult LoadMore(string filter = "all", int startLoad = 0)
+        {
+            IQueryable<BlogPost> query = _context.BlogPosts.Include(bp => bp.Category).OrderByDescending(bp => bp.CreatedDate);
+
+            //filter by category
+            if(filter != "all")
+            {
+                query = query.Where(bp => bp.Category.Name == filter);
+            }
+            //get next 'page'
+            query = query.Skip(startLoad).Take(_pageSize);
+            var vm = new BlogViewModel { };
+            vm.Posts = query.ToList();
+            return Json(vm.Posts);
         }
     }
 }
